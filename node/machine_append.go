675437ac37b1bb12a79cad5afca69911a988d0e6
David Reed <dreed@drift.com>
2019-12-06 18:59:40 -0500

    Set leader on append

--- node/machine_append.go
+++ node/machine_append.go
@@ -5,9 +5,12 @@ import "github.com/djreed/raft/data"
 // TODO if we get a term > currentTerm, convert to Follower, set term to higher
 func HandleAppendEntries(n *Node, appendEntries data.AppendEntries) data.MessageList {
 	n.ResetElectionTimeout()
+	n.SetLeader(appendEntries.Leader)
+	// TODO
 	return nil
 }
 
 func HandleAppendEntriesResponse(n *Node, appendRes data.AppendEntriesResponse) data.MessageList {
+	// TODO
 	return nil
 }
