06d97d67d79760248c7da8d9d8d96079387e86f3
David Reed <dreed@drift.com>
2019-12-07 19:16:31 -0500

    Longer timeout

--- node/helper_constants.go
+++ node/helper_constants.go
@@ -3,8 +3,8 @@ package node
 import "time"
 
 const (
-	electRange    = 200
-	electBase     = time.Duration(300 * time.Millisecond)
+	electRange    = 250
+	electBase     = time.Duration(500 * time.Millisecond)
 	heartbeatBase = electBase / 5
 )
 
